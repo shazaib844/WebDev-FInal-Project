f0c4bfb8f3ecedbc254b3ae799dff559521d639c
shazaib844 <134206757+shazaib844@users.noreply.github.com>
2024-01-31 16:30:27 +0500

    Add files via upload

---
+++ backend/urls.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from .views import index
+
+urlpatterns = [
+    path('', index),
+    path('admin/', admin.site.urls),
+    path('', include('api.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
